3b73414c547159ea5112e10c6d2a82f9144f85ab
gielow <andre.gielow@gmail.com>
2016-03-10 22:27:43 -0300

    Entites project

---
+++ TC_WebShopCaseMVC/Controllers/ArticleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TC_WebShopCaseMVC.Controllers
+{
+    public class ArticleController : ApiController
+    {
+        // GET api/article
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/article/5
+        public string Get(int id)
+        {
+            return "value";
+
+        }
+
+        // POST api/article
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/article/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/article/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TC_WebShopCaseMVC/Models/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TC_WebShopCaseMVC.Models
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public int HouseNumber { get; set; }
+    }
+}
