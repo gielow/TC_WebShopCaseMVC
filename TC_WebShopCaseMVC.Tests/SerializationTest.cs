baa421a6fea1d86aab3bd0b73034a23c2326982d
gielow <andre.gielow@gmail.com>
2016-03-11 01:09:32 -0300

    Entities, xml reader, Article controller and view

---
+++ TC_WebShopCaseMVC.Tests/SerializationTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TC_WebShopCaseMVC.Entities;
+using System.IO;
+
+namespace TC_WebShopCaseMVC.Tests
+{
+    /// <summary>
+    /// Summary description for SerializationTest
+    /// </summary>
+    [TestClass]
+    public class SerializationTest
+    {
+        private const string articlePath = @"E:\VSWorkspace\TC_WebShopCaseMVC\TC_WebShopCaseMVC\App_Data\articles.xml";
+        public SerializationTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Serialize()
+        {
+            var articles = new List<Article>();
+
+            for (int i = 1; i <= 50; i++)
+            {
+                articles.Add(new Article()
+                {
+                    Address = "Primavera st",
+                    City = "Blumenau",
+                    Email = "andre.gielow@gmail.com",
+                    FirstName = "André " + i.ToString("0#"),
+                    LastName = "Gielow",
+                    HouseNumber = 291,
+                    ZipCode = "89057-036",
+                    Title = "Mr."
+                });
+            }
+            
+            Serializer.SerializeObject<List<Article>>(articles, articlePath);
+            Assert.IsTrue(File.Exists(articlePath));
+        }
+
+        [TestMethod]
+        public void Deserialize()
+        {
+            var articles = Serializer.DeSerializeObject<List<Article>>(articlePath);
+
+            Assert.AreEqual(1, articles.Count);
+        }
+    }
+}
--- TC_WebShopCaseMVC/Controllers/ArticleController.cs
+++ TC_WebShopCaseMVC/Controllers/ArticleController.cs
@@ -1,40 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
+using TC_WebShopCaseMVC.Entities;
 
 namespace TC_WebShopCaseMVC.Controllers
 {
-    public class ArticleController : ApiController
+    public class ArticleController : Controller
     {
-        // GET api/article
-        public IEnumerable<string> Get()
+        //
+        // GET: /Article/
+        public ActionResult Index(int? pageNumber, int? pageSize)
         {
-            return new string[] { "value1", "value2" };
+            if (!pageNumber.HasValue)
+                pageNumber = 1;
+
+            if (!pageSize.HasValue)
+                pageSize = 10;
+
+            var reader = new XMLReader(HttpContext.Server.MapPath("~/App_Data"));
+            var articles = reader.GetArticles().Skip((pageNumber.Value - 1) * pageSize.Value).Take(pageSize.Value);
+
+            return View(articles);
         }
 
-        // GET api/article/5
-        public string Get(int id)
-        {
-            return "value";
+        //
+        // GET: /Article/Details/5
 
+        public ActionResult Details(int id)
+        {
+            return View();
         }
 
-        // POST api/article
-        public void Post([FromBody]string value)
+        //
+        // GET: /Article/Create
+
+        public ActionResult Create()
         {
+            return View();
         }
 
-        // PUT api/article/5
-        public void Put(int id, [FromBody]string value)
+        //
+        // POST: /Article/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
         {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
-        // DELETE api/article/5
-        public void Delete(int id)
+        //
+        // GET: /Article/Edit/5
+
+        public ActionResult Edit(int id)
         {
+            return View();
+        }
+
+        //
+        // POST: /Article/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Article/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Article/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
